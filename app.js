135c80f5a3c719aba1ee315dbeca05b3fe6d0bfe
annejones817 <annejones817@gmail.com>
2016-10-11 17:00:57 -0700

    Added functionality to store user's responses in preferences object

--- app.js
+++ app.js
@@ -21,25 +21,37 @@ $(document).ready(function(event){
 	function askQuestions() {
 		if (questionCounter<questions.length) {
 			$('.ask-question').removeClass('hidden').prepend('<h3 class="question">' + questions[questionCounter].question + '</h3>'); 
-			questionCounter++;
 		} else {
 			$('.ask-question').remove();
 			$('main').append('<p>Thanks for reviewing. Now I need to build the rest of the app!</p>')
 		}	
 	}
 
+///////Record Answers////////////
+	function recordAnswer(taste, preference) {
+		preferences[taste] = preference;
+	}
+
+
 ///////////Event Listeners//////////////
+////Click "start"/////
 	$('.start').click(function(event){
 		$('.welcome').remove();
 		$('.start').remove();
 		askQuestions(); 
 	});
 
+/////Click "next"///////
 	$('.ask-question').submit(function(event){
 		event.preventDefault();
+		//Check whether user has selected an answer & behave accordingly
 		if (($('input[name=answer]:checked').val()) == undefined) {
 			$('.ask-question').prepend('<h2 class="require-answer">Please select Aye or Nay.</h2>');
 		} else {
+			recordAnswer(questions[questionCounter].taste, $('input[name=answer]:checked').val());
+			console.log(preferences);
+			questionCounter++;
+			console.log(questionCounter);
 			$('.question').remove();
 			$('.require-answer').remove();
 			$('input[name=answer]:checked').prop('checked', false);
